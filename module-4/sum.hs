7cf88e55b6e9056b41dab60f51675df84cfd4910
Umbrien <kalugin.serej@gmail.com>
2023-03-11 03:16:26 +0200

    check 22.2

--- module-4/sum.hs
+++ module-4/sum.hs
@@ -12,13 +12,16 @@ main1 = do
   args <- getArgs
   mapM_ putStrLn args
 
+myReplicateM :: Monad m => Int -> m a -> m [a]
+myReplicateM n f = mapM (\_ -> f) [1 .. n]
+
 main :: IO ()
 main = do
   args <- getArgs
   let linesToRead = if length args > 0
                       then read $ head args
                       else 0 :: Int
-  numbers <- replicateM linesToRead getLine
+  numbers <- myReplicateM linesToRead getLine
   let ints = map read numbers :: [Int]
   putStrLn "- - -"
   print $ sum ints
