8719658002dc6a4b650b24f58c63027e0497b67b
Serhii <kaluhin.dev@gmail.com>
2024-06-30 02:39:13 +0300

    first useful program

---
+++ my-programs/rh.hs
@@ -0,0 +1,13 @@
+import System.Directory (removeFile)
+import System.Environment (getArgs)
+
+genFileVariants :: String -> [String]
+genFileVariants s = [s, s ++ ".o", s ++ ".hi"]
+
+main :: IO ()
+main = do
+  args <- getArgs
+  let filesToRemove = concatMap genFileVariants args
+  mapM_ removeFile filesToRemove
+
+-- print filesToRemove
